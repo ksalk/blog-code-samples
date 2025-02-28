714a7c7d7f63b66fa0dbfab2fc0600e92cf1d790
Konrad Sałkowski <konrad.salkowski@gmail.com>
2025-02-28 13:16:47 +0100

    Extract hardcoded port value

--- src/2025/test-containers/TestContainersExample.Tests/CustomContainerIntegrationTestBase.cs
+++ src/2025/test-containers/TestContainersExample.Tests/CustomContainerIntegrationTestBase.cs
@@ -16,20 +16,21 @@ public class CustomContainerIntegrationTestBase : IAsyncLifetime
     private readonly string dbUsername = "testuser";
     private readonly string dbPassword = "testpassword";
     private readonly string dbName = "testdb";
+    private static int port => 5432;
 
     public async Task InitializeAsync()
     {
         _postgresContainer = new ContainerBuilder()
             .WithImage("postgres:latest")
-            .WithPortBinding(5432, true)
+            .WithPortBinding(port, true)
             .WithEnvironment("POSTGRES_USER", dbUsername)
             .WithEnvironment("POSTGRES_PASSWORD", dbPassword)
             .WithEnvironment("POSTGRES_DB", dbName)
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(port))
             .Build();
 
         await _postgresContainer.StartAsync();
-        var connectionString = $"Host={_postgresContainer.Hostname};Port={_postgresContainer.GetMappedPublicPort(5432)};Database={dbName};Username={dbUsername};Password={dbPassword}";
+        var connectionString = $"Host={_postgresContainer.Hostname};Port={_postgresContainer.GetMappedPublicPort(port)};Database={dbName};Username={dbUsername};Password={dbPassword}";
 
         _factory = new WebApplicationFactory<Program>()
             .WithWebHostBuilder(builder =>
